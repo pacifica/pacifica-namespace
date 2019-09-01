9eec86196558eeea43b0635ac1106123a11dafc7
David Brown <dmlb2000@gmail.com>
2019-09-01 08:00:16 -0700

    Remove example to only have namespace (#1)
    * Remove example to only have namespace
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>
    
    * Fix the password
    
    Signed-off-by: David Brown <dmlb2000@gmail.com>

--- setup.py
+++ setup.py
@@ -2,28 +2,19 @@
 # -*- coding: utf-8 -*-
 """Setup and install the pacifica service."""
 from os import path
-try:  # pip version 9
-    from pip.req import parse_requirements
-except ImportError:
-    from pip._internal.req import parse_requirements
 from setuptools import setup, find_packages
 
-# parse_requirements() returns generator of pip.req.InstallRequirement objects
-INSTALL_REQS = parse_requirements('requirements.txt', session='hack')
-
 setup(
-    name='pacifica-service',
+    name='pacifica-namespace',
     use_scm_version=True,
     setup_requires=['setuptools_scm'],
-    description='Pacifica Example Library',
-    url='https://github.com/pacifica/template-repository/',
+    description='Pacifica Namespace Package',
+    url='https://github.com/pacifica/pacifica-namespace/',
     long_description=open(path.join(
         path.abspath(path.dirname(__file__)),
         'README.md')).read(),
     long_description_content_type='text/markdown',
     author='David Brown',
     author_email='dmlb2000@gmail.com',
-    packages=find_packages(),
-    namespace_packages=['pacifica'],
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    packages=find_packages()
 )
